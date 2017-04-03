e1d894cfe0d28ff297d0b835eb01ee8ddf8f8835
James Forster <james.forster@digital.hmrc.gov.uk>
2017-04-03 17:52:28 +0100

    Added MapService methods for sector and universe.

---
+++ app/controllers/MapController.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import com.google.inject.{Inject, Singleton}
+import play.api.mvc.Controller
+
+/**
+  * Created by james-forster on 03/04/17.
+  */
+
+@Singleton
+class MapController @Inject()() extends Controller {
+
+}
---
+++ app/services/MapService.scala
@@ -0,0 +1,28 @@
+package services
+
+import com.google.inject.{Inject, Singleton}
+import connectors.MongoConnector
+import models.coordinates.SectorCoordinateModel
+import models.universe.{SectorModel, UniverseModel}
+import play.api.libs.json.Json
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+/**
+  * Created by james-forster on 03/04/17.
+  */
+
+@Singleton
+class MapService @Inject()(mongoConnector: MongoConnector) {
+
+  def getUniverse(universeName: String): Future[UniverseModel] = {
+    mongoConnector.getAllEntries[SectorModel](universeName).map { sectors =>
+      UniverseModel(sectors)
+    }
+  }
+
+  def getSector(universeName: String, sectorCoordinateModel: SectorCoordinateModel): Future[Option[SectorModel]] = {
+    mongoConnector.getEntry[SectorModel](universeName, "sectorCoordinateModel", Json.toJson(sectorCoordinateModel))
+  }
+}
